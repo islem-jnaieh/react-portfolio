84cc39ac793eff578cf707eacbe96d27ebea2bf7
islem-jnaieh <islemjnaieh07@gmail.com>
2022-08-01 14:50:21 +0200

    init

---
+++ src/components/experience/experience.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './exerience.css'
+const experience = () => {
+  return (
+    <div>experience</div>
+  )
+}
+
+export default experience
---
+++ src/components/testimonials/testimonials.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './tesstimonials.css'
+const testimonials = () => {
+  return (
+    <div>testimonials</div>
+  )
+}
+
+export default testimonials
